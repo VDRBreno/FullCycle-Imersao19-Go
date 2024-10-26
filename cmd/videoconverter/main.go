9b0439cd6975520f81bfff30635c62873889f52b
VDRBreno <vdr.breno0149@outlook.com>
2024-10-26 16:34:24 -0300

    First commit

---
+++ cmd/videoconverter/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log/slog"
+	"os"
+
+	"fc-imersao19/internal/converter"
+	"fc-imersao19/internal/rabbitmq"
+
+	_ "github.com/lib/pq"
+	"github.com/streadway/amqp"
+)
+
+func connectPostgres() (*sql.DB, error) {
+	user := getEnvOrDefault("POSTGRES_USER", "user")
+	password := getEnvOrDefault("POSTGRES_PASSWORD", "password")
+	dbname := getEnvOrDefault("POSTGRES_DB", "converter")
+	host := getEnvOrDefault("POSTGRES_HOST", "postgres")
+	sslmode := getEnvOrDefault("POSTGRES_SSLMODE", "disable")
+
+	connStr := fmt.Sprintf("user=%s password=%s dbname=%s host=%s sslmode=%s", user, password, dbname, host, sslmode)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		slog.Error("Error connecting to database", slog.String("connStr", connStr))
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		slog.Error("Error pinging database", slog.String("connStr", connStr))
+		return nil, err
+	}
+
+	slog.Info("Connected to Postgres successfully")
+	return db, nil
+}
+
+func getEnvOrDefault(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
+}
+
+func main() {
+	db, err := connectPostgres()
+	if err != nil {
+		panic(err)
+	}
+
+	rabbitMQURL := getEnvOrDefault("RABBITMQ_URL", "amqp://guest:guest@rabbitmq:5672/")
+	rabbitClient, err := rabbitmq.NewRabbitClient(rabbitMQURL)
+	if err != nil {
+		panic(err)
+	}
+	slog.Info("Connected to RabbitMQ")
+	defer rabbitClient.Close()
+
+	conversionExch := getEnvOrDefault("CONVERSION_EXCHANGE", "conversion_exchange")
+	queueName := getEnvOrDefault("CONVERSION_QUEUE", "video_conversion_queue")
+	conversionKey := getEnvOrDefault("CONVERSION_KEY", "conversion")
+	confirmationKey := getEnvOrDefault("CONFIRMATION_KEY", "finish-confirmation")
+	confirmationQueue := getEnvOrDefault("CONFIRMATION_QUEUE", "video_confirmation_queue")
+
+	vc := converter.NewVideoConverter(rabbitClient, db)
+	// vc.Handle([]byte(`{"video_id": 1, "path": "/media/uploads/1"}`))
+
+	msgs, err := rabbitClient.ConsumeMessages(conversionExch, conversionKey, queueName)
+	if err != nil {
+		slog.Error("Failed to consume messages", slog.String("error", err.Error()))
+	}
+
+	for d := range msgs {
+		go func(delivery amqp.Delivery) {
+			vc.Handle(delivery, conversionExch, confirmationKey, confirmationQueue)
+		}(d)
+	}
+}
---
+++ internal/converter/task.go
@@ -0,0 +1,166 @@
+package converter
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fc-imersao19/internal/rabbitmq"
+	"fmt"
+	"log/slog"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"sort"
+	"strconv"
+	"time"
+
+	"github.com/streadway/amqp"
+)
+
+type VideoConverter struct {
+	db             *sql.DB
+	rabbitmqClient *rabbitmq.RabbitClient
+}
+
+func NewVideoConverter(rabbitmqClient *rabbitmq.RabbitClient, db *sql.DB) *VideoConverter {
+	return &VideoConverter{
+		rabbitmqClient: rabbitmqClient,
+		db:             db,
+	}
+}
+
+type VideoTask struct {
+	VideoID int    `json:"video_id"`
+	Path    string `json:"path"`
+}
+
+func (vc *VideoConverter) Handle(d amqp.Delivery, conversionExch, confirmationKey, confirmationQueue string) {
+	var task VideoTask
+	err := json.Unmarshal(d.Body, &task)
+	if err != nil {
+		vc.logError(task, "failed to unmarshal task", err)
+		return
+	}
+
+	if IsProcessed(vc.db, task.VideoID) {
+		slog.Warn("Video already processed", slog.Int("video_id", task.VideoID))
+		d.Ack(false)
+		return
+	}
+
+	err = vc.processVideo(&task)
+	if err != nil {
+		vc.logError(task, "failed to process video", err)
+		return
+	}
+
+	err = MarkProcessed(vc.db, task.VideoID)
+	if err != nil {
+		vc.logError(task, "failed to mark video as processed", err)
+		return
+	}
+	d.Ack(false)
+	slog.Info("Video marked as processed", slog.Int("video_id", task.VideoID))
+
+	confirmationMessage := []byte(fmt.Sprintf(`{"video_id": %d, "path":"%s"}`, task.VideoID, task.Path))
+	err = vc.rabbitmqClient.PublishMessage(conversionExch, confirmationKey, confirmationQueue, confirmationMessage)
+}
+
+func (vc *VideoConverter) processVideo(task *VideoTask) error {
+	mergedFile := filepath.Join(task.Path, "merged.mp4")
+	mpegDashPath := filepath.Join(task.Path, "mpeg-dash")
+
+	slog.Info("Creating mpeg-dash directory", slog.String("path", task.Path))
+	err := os.MkdirAll(mpegDashPath, os.ModePerm)
+	if err != nil {
+		vc.logError(*task, "failed to create mpeg-dash directory", err)
+		return err
+	}
+
+	slog.Info("Merging chunks", slog.String("path", task.Path))
+	err = vc.mergeChunks(task.Path, mergedFile)
+	if err != nil {
+		vc.logError(*task, "failed to merge chunks", err)
+		return err
+	}
+
+	slog.Info("Converting video to mpeg-dash", slog.String("path", task.Path))
+	ffmpegCmd := exec.Command(
+		"ffmpeg", "-i", mergedFile,
+		"-f", "dash",
+		filepath.Join(mpegDashPath, "output.mpd"),
+	)
+
+	output, err := ffmpegCmd.CombinedOutput()
+	if err != nil {
+		vc.logError(*task, "failed to convert video to mpeg-dash, output: "+string(output), err)
+		return err
+	}
+	slog.Info("Video converted to mpeg-dash", slog.String("path", mpegDashPath))
+
+	slog.Info("Removing merged file", slog.String("path", mergedFile))
+	err = os.Remove(mergedFile)
+	if err != nil {
+		vc.logError(*task, "failed to remove merged file", err)
+		return err
+	}
+
+	return nil
+}
+
+func (vc *VideoConverter) logError(task VideoTask, message string, err error) {
+	errorData := map[string]any{
+		"video_id": task.VideoID,
+		"error":    message,
+		"details":  err.Error(),
+		"time":     time.Now(),
+	}
+
+	serializedError, _ := json.Marshal(errorData)
+	slog.Error("Processing error", slog.String("error_details", string(serializedError)))
+
+	RegisterError(vc.db, errorData, err)
+}
+
+func (vc *VideoConverter) extractNumber(fileName string) int {
+	regex := regexp.MustCompile(`\d+`)
+	numStr := regex.FindString(filepath.Base(fileName))
+	num, err := strconv.Atoi(numStr)
+	if err != nil {
+		return -1
+	}
+	return num
+}
+
+func (vc *VideoConverter) mergeChunks(inputDir, outputFile string) error {
+	chunks, err := filepath.Glob(filepath.Join(inputDir, "*.chunk"))
+	if err != nil {
+		return fmt.Errorf("failed to find chunks: %v", err)
+	}
+
+	sort.Slice(chunks, func(i, j int) bool {
+		return vc.extractNumber(chunks[i]) < vc.extractNumber(chunks[j])
+	})
+
+	output, err := os.Create(outputFile)
+	if err != nil {
+		return fmt.Errorf("failed to create output file: %v", err)
+	}
+	defer output.Close()
+
+	for _, chunk := range chunks {
+		input, err := os.Open(chunk)
+		if err != nil {
+			return fmt.Errorf("failed to open chunk: %v", err)
+		}
+
+		_, err = output.ReadFrom(input)
+		if err != nil {
+			return fmt.Errorf("failed to write chunk %s to merged file: %v", chunk, err)
+		}
+
+		input.Close()
+	}
+
+	return nil
+}
